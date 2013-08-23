5ea1b0be316073d6b3775a819aa212960caa6fd6
Pieter Slabbert <blob626@gmail.com>
2013-08-23 20:45:12 +0200

    add basic incremental movement in four directions
    very rough movement for now, uses w,a,s,d.

--- square-man.scm
+++ square-man.scm
@@ -11,7 +11,7 @@
 (open-window *window-width* *window-height*)
 
 ;; Load a sprite and center it on the screen.
-(define sprite
+(define *sprite*
   (load-sprite "/home/zan-xhipe/projects/square-man/sprite.png"
                #:position (vector (/ *window-width* 2)
                                   (/ *window-height* 2))))
@@ -31,10 +31,17 @@
   (cond ((any-equal? key 'escape 'q)
          (quit-demo))
 	((any-equal? key 'a)
-	 (move-by sprite (vector -10 0)))))
+	 (move-by *sprite* (vector -10 0)))
+	((any-equal? key 'd)
+	 (move-by *sprite* (vector 10 0)))
+	((any-equal? key 's)
+	 (move-by *sprite* (vector 0 -10)))
+	((any-equal? key 'w)
+	 (move-by *sprite* (vector 0 10)))))
+
 ;; Draw our sprite
 (define (render)
-  (draw-sprite sprite))
+  (draw-sprite *sprite*))
 
 ;; Register hooks. Lambdas are used as "trampolines" so that render
 ;; and key-down can be redefined later and the hooks will call the
