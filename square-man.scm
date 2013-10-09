21aa9888946fe5b6b354b34848ee2cdf8ba55b93
Pieter Slabbert <blob626@gmail.com>
2013-10-09 22:10:34 +0200

    Rename game title

--- square-man.scm
+++ square-man.scm
@@ -1,4 +1,5 @@
 (load "entity.scm")
+(load "game-state.scm")
 
 (use-modules (srfi srfi-9)
 	     (2d sprite)
@@ -6,49 +7,30 @@
              (2d vector2)
 	     (2d helpers)
 
-	     (square-man entity))
-
-(define-record-type <game-state>
-  (make-game-state player)
-  game-state?
-  (player game-player))
+	     (square-man entity)
+	     (square-man game-state))
 
 (define (player-sprite)
   (load-sprite "images/player.png"
                #:position (vector2 320 240)))
 
-(define (draw state)
-  (draw-entity (game-player state)))
-
-(define (update state)
-  (update-entity! (game-player state)))
-
-(define (make-move-entity x y)
-  (lambda (entity speed)
-    (set-entity-velocity! entity (vector2 (* speed x) (* speed y)))))
-
-(define move-left! (make-move-entity -1 0))
-(define move-right! (make-move-entity 1 0))
-(define move-up! (make-move-entity 0 -1))
-(define move-down! (make-move-entity 0 1))
-
 (define *player-speed* 1)
 
 (define (key-down state key mod unicode)
   (cond ((any-equal? key 'a)
-	 (move-left! (game-player state) *player-speed*))
+	 (move-left! (player state) *player-speed*))
 	
 	((any-equal? key 'd)
-	 (move-right! (game-player state) *player-speed*))
+	 (move-right! (player state) *player-speed*))
 	
 	((any-equal? key 's)
-	 (move-down! (game-player state) *player-speed*))
+	 (move-down! (player state) *player-speed*))
 	
 	((any-equal? key 'w)
-	 (move-up! (game-player state) *player-speed*))))
+	 (move-up! (player state) *player-speed*))))
 
-(define-scene demo
-  #:title  "Demo"
+(define-scene scene-1
+  #:title  "Sqaure man scene"
   #:update (lambda (state) (update state))
   #:draw   (lambda (state) (draw state))
   #:events (append
@@ -57,8 +39,8 @@
 			     (key-down state key mod unicode)))))
   #:state  (make-game-state (make-entity (player-sprite) (vector2 0 0))))
 
-(define-game simple
-  #:title       "Simple Demo"
-  #:first-scene demo)
+(define-game square-man-game
+  #:title       "Square Man"
+  #:first-scene scene-1)
 
-(run-game simple)
+(run-game square-man-game)
