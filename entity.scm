7c09073f80e1f210583dae05b644007967ad8791
Pieter Slabbert <blob626@gmail.com>
2013-10-10 22:03:57 +0200

    Add types to entity for use in collision detection

--- entity.scm
+++ entity.scm
@@ -8,7 +8,9 @@
 
 	    update-entity!
 	    draw-entity
-	    
+
+	    entity-type
+	    set-entity-type!
 	    entity-sprite
 	    set-entity-sprite!
 	    entity-radius
@@ -26,8 +28,9 @@
 
 
 (define-record-type <entity>
-  (make-entity sprite radius velocity)
+  (make-entity type sprite radius velocity)
   entity?
+  (type entity-type set-entity-type!)
   (sprite entity-sprite set-entity-sprite!)
   (radius entity-radius set-entity-radius!)
   (velocity entity-velocity set-entity-velocity!))
