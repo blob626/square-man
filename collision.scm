eb172ca40c0e4e014d747a6174c07233ba05b358
Pieter Slabbert <blob626@gmail.com>
2013-10-10 21:40:47 +0200

    Add basic grid based collision detection

---
+++ collision.scm
@@ -0,0 +1,101 @@
+(define-module (square-man collision)
+  #:use-module (srfi srfi-1)
+  #:use-module (2d vector2)
+  #:use-module (square-man entity)
+  #:export (make-collision-detection
+	    make-collision-handler
+	    make-collision-system
+	    make-grid
+	    square
+	    distance-square))
+
+(define (make-collision-detection narrow? broad)
+  (define (narrow-phase possible-collisions)
+    (pair-wise-filter-map
+     (lambda (a b)
+       (and (narrow? a b) (list a b)))
+     possible-collisions))
+  (define (value-or-#f value)
+    (if (null? value)
+	#f
+	value))
+  (lambda (objects)
+    (apply append
+	   (filter-map
+	    (lambda (possible)
+	      (value-or-#f (narrow-phase possible)))
+	    (broad objects)))))
+
+(define (make-collision-handler handler)
+  (lambda (collisions)
+    (for-each handler collisions)))
+
+;;; objects is the function which when give state returns a list of all collidable objects
+(define (make-collision-system objects broad narrow handler)
+  (define detect (make-collision-detection narrow broad))
+  (define handle (make-collision-handler handler))
+  (lambda (state)
+    (handle (detect (objects state)))))
+
+(define (make-grid window-height window-width size)
+  (define (make-empty-grid)
+    (make-array '()
+		(+ (ceiling (/ window-height size)) 1)
+		(+ (ceiling (/ window-width size)) 1)))
+  
+  (define (position->gridtile position)
+    (vector2 (ceiling (/ (vx position) size))
+	     (ceiling (/ (vy position) size))))
+
+  (let ((grid (make-empty-grid)))
+   
+    (define (cell-ref position)
+      (array-ref grid (vy position) (vx position)))
+    (define (set-cell! value position)
+      (array-set! grid value (vy position) (vx position)))
+    (define (insert! entity)
+      (let ((position (position->gridtile (entity-position entity))))
+	(set-cell! (cons entity (cell-ref position)) position)))
+    (define (insert-all! lst)
+      (clear-grid!)
+      (for-each insert! lst))
+    (define (clear-grid!)
+      (set! grid (make-empty-grid)))
+    (define (possible-collisions)
+      (let ((possible '()))
+	(array-for-each (lambda (cell)
+			  (set! possible (cons cell possible)))
+			grid)
+	possible))
+    (lambda (objects)
+      (insert-all! objects)
+      (possible-collisions))))
+
+(define (pair-wise-map function lst)
+  (define (helper _lst accumulator)
+    (if (null? _lst)
+	accumulator
+	(helper (cdr _lst)
+		(append accumulator
+			(map (lambda (x)
+			       (function (car _lst) x))
+			     (cdr _lst))))))
+  (helper lst (list)))
+
+(define (pair-wise-filter-map function lst)
+  (define (helper _lst accumulator)
+    (if (null? _lst)
+	accumulator
+	(helper (cdr _lst)
+		(append accumulator
+			(filter-map (lambda (x)
+			       (function (car _lst) x))
+				    (cdr _lst))))))
+  (helper lst (list)))
+
+(define (square x)
+  (* x x))
+
+(define (distance-square a b)
+  (+ (square (- (vx a) (vx b)))
+     (square (- (vy a) (vy b)))))
--- square-man.scm
+++ square-man.scm
@@ -12,9 +12,33 @@
 	     (square-man collision)
 	     (square-man game-state))
 
+(define *screen-width* 640)
+(define *screen-height* 480)
+
 (define (player-sprite)
   (load-sprite "images/player.png"
                #:position (vector2 320 240)))
+
+(define (food-sprite position)
+  (load-sprite "images/bullet.png"
+	       #:position position))
+
+(define grid (make-grid *screen-height* *screen-width* 64))
+
+(define (collide? a b)
+  (< (distance-square (entity-position a) (entity-position b))
+     (square (+ (entity-radius a) (entity-radius b)))))
+
+(define (collision-handler collision)
+  (and (entity? (car collision))
+       (entity? (cadr collision))
+       (quit)))
+
+(define (state-objects state)
+  (cons (player state) (food state)))
+
+(define collision-system
+  (make-collision-system state-objects grid collide? collision-handler))
 
 (define *player-speed* 1)
 
@@ -33,16 +57,21 @@
 
 (define-scene scene-1
   #:title  "Sqaure man scene"
-  #:update update
+  #:update (lambda (state)
+	     (update state)
+	     (collision-system state))
   #:draw   draw
   #:events (append
 	    (default-scene-events)
 	    `((key-down . ,(lambda (state key mod unicode)
 			     (key-down state key mod unicode)))))
-  #:state  (make-game-state (make-entity (player-sprite) (vector2 0 0))))
+  #:state  (make-game-state
+	    (make-entity (player-sprite) 16 (vector2 0 0))
+	    (list (make-entity (food-sprite (vector2 10 10)) 10 (vector2 0 0)))))
 
 (define-game square-man-game
   #:title       "Square Man"
+  #:resolution (vector2 *screen-width* *screen-height*)
   #:first-scene scene-1)
 
 (run-game square-man-game)
