40634153ee85a1ba51d56cc039a9abbd74a90d81
Pieter Slabbert <blob626@gmail.com>
2013-10-16 21:06:52 +0200

    Add comments and documentation

--- collision.scm
+++ collision.scm
@@ -7,11 +7,17 @@
 	    make-collision-handler
 	    make-collision-system
 	    make-grid
-	    make-collide?
-	    square
-	    distance-square))
+	    make-collide?))
+
+;;; A collision is a list of length two that containss the two colliding
+;;; objects. (object1 object2)
 
 (define (make-collision-detection narrow? broad)
+  "Returns a new collision detector that takes a list of objects and returns a list of collisions. NARROW? is a function that returns
+#t if two objects are actually colliding, and #f otherwise. Its takes a 
+list of the two potentially colliding objects as its argument. BROAD is a 
+function that takes a list of bjects and returns a list of all potential
+collisions between them."
   (define (narrow-phase possible-collisions)
     (pair-wise-filter-map
      (lambda (a b)
@@ -28,11 +34,17 @@
 	      (value-or-#f (narrow-phase possible)))
 	    (broad objects)))))
 
+;;; A collision handler takes a list of collisions and reacts to them
 (define* (make-collision-handler #:optional #:key
 				(start (lambda (collisions) #f))
 				(during (lambda (collisions) #f))
 				(end (lambda (collisions) #f)))
-  "Returns a new collision handler"
+  "Returns a new collision handler. START is a function that is called
+when two objects first collide. DURING is a function that is called if two 
+objects remain in contact after the initial collision. END is a
+function that is called when two objects that were colliding stop colliding.
+The handler functions (START DURING END) take a list of length two as their 
+arguments, that contains the two colliding objects."
   (let ((previous-collisions (list)))
  
     (define (remove-previous-collision! collision)
@@ -57,17 +69,29 @@
 ;;; broad is the broad-phase collision detection. this calls the spatial partioning to find possible collision.
 ;;; handler is the function that determines what happens when particular objects collide
 (define (make-collision-system objects sprite-proc broad handler)
+  "Returns a new collision system. It takes a state and responds to
+all the collisions that occur in the state. OBJECTS is a function that
+accepts a state and returns all the objects that can collide. SPRITE-PROC 
+is a function that returns the sprite of an object in the state. BROAD is 
+a function that takes a list of objects and returns a list of possible 
+collision. HANDLER is a function that takes a list of collisions and 
+responds to them."
   (define collide? (make-collide? sprite-proc))
   (define detect (make-collision-detection collide? broad))
   (lambda (state)
     (handler (delete-duplicates (detect (objects state))))))
 
 ;;; make a grid-based collision detection
-(define (make-grid sprite-proc window-height window-width size)
+(define (make-grid sprite-proc grid-height grid-width size)
+  "Returns a new grid that takes a list of objects and returns 
+all the possible collisions in it. SPRITE-PROC is a function that 
+takes an object and returns its sprite. GRID-HEIGHT is the height in 
+pixels of the grid. GRID-WIDTH is the width in pixels of the grid. 
+SIZE is the size of a grid cell in pixels. "
   (define (make-empty-grid)
     (make-array '()
-		(+ (ceiling (/ window-height size)) 1)
-		(+ (ceiling (/ window-width size)) 1)))
+		(+ (ceiling (/ grid-height size)) 1)
+		(+ (ceiling (/ grid-width size)) 1)))
   
   (define (position->gridtile position)
     (vector2 (ceiling (/ (vx position) size))
@@ -109,6 +133,8 @@
       (possible-collisions))))
 
 (define (pair-wise-map function lst)
+  "Calls FUNCTION on all pairwise combinations of elements in LST. 
+Returns a list of the results."
   (define (helper _lst accumulator)
     (if (null? _lst)
 	accumulator
@@ -120,6 +146,9 @@
   (helper lst (list)))
 
 (define (pair-wise-filter-map function lst)
+  "Calls FUNCTION on all pairwise combinations of elements in LST. 
+Returns a list of the results, if a result is #f its not included 
+in the list."
   (define (helper _lst accumulator)
     (if (null? _lst)
 	accumulator
@@ -130,14 +159,11 @@
 				    (cdr _lst))))))
   (helper lst (list)))
 
-(define (square x)
-  (* x x))
-
-(define (distance-square a b)
-  (+ (square (- (vx a) (vx b)))
-     (square (- (vy a) (vy b)))))
-
 (define (make-corners sprite-proc)
+  "Returns a function that takes an object and returns a corner of 
+its sprite. Use the symbols 'bottom-right 'bottom-left 'top-right 
+'top-left. SPRITE-PROC is a function that takes a object and returns 
+its sprite."
   (lambda (entity)
     (let* ((sprite (sprite-proc entity))
 	   (centre (sprite-position sprite))
@@ -155,6 +181,9 @@
 	  ((top-left) (corner - -)))))))
 
 (define (make-collide? sprite-proc)
+  "Returns a function that takes two objects an returns #t if they 
+collide and #f otherwise. SPRITE-PROC is a function that takes an object 
+and returns its sprite."
   (define corner (make-corners sprite-proc))
   (lambda (a b)
     (let ((top-left-a ((corner a) 'top-left))
